fa2b7b2474767bd20091791dc2f9bfb412ec2d83
Wagubi Brian <wagubib@gmail.com>
2019-10-02 10:44:53 +0300

    Add routes for the meals to the api-gateway.

--- api/controllers/gatewayController.js
+++ api/controllers/gatewayController.js
@@ -52,6 +52,36 @@ class GatewayController {
         }
     }
 
+    async getMeals(req, res) {
+        const { body } = req;
+        try {
+            const output = await axios.get('http://0.0.0.0:3071/api/v1/meals');
+            res.send(output.data);
+        } catch (err) {
+            res.send(err.response.data);
+        }
+    }
+
+    async addMeal(req, res) {
+        const { body } = req;
+        try {
+            const output = await axios.post('http://0.0.0.0:3071/api/v1/meals', { ...body });
+            res.send(output.data);
+        } catch (err) {
+            res.send(err.response.data);
+        }
+    }
+
+    async deleteMeal(req, res) {
+        const { id } = req.params;
+        try {
+            const output = await axios.delete('http://0.0.0.0:3071/api/v1/meals/' + id);
+            res.send(output.data);
+        } catch (err) {
+            res.send(err.response.data);
+        }
+    }
+
 }
 
 export default new GatewayController();
