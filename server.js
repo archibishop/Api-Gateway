b4c5048d0fc15115e51dc12776bd96018189fbbb
Wagubi Brian <36948493+archibishop@users.noreply.github.com>
2019-11-05 19:49:46 +0300

    refactor api-gateway. (#4)

--- server.js
+++ server.js
@@ -1,24 +1,54 @@
+/* eslint-disable no-console */
 import express from 'express';
-import bodyParser from 'body-parser';
-import axios from 'axios';
-import gatewayRoutes from './api/routes/gatewayRoutes';
+import Docker from 'dockerode';
+import { EventEmitter } from 'events';
+import proxy from 'http-proxy-middleware';
+import middlewareObj from './api/middleware/isAuthroized';
 
 // Server
 const app = express();
 
+const docker = new Docker();
+const mediator = new EventEmitter();
+
+
+const containerMetaData = [];
 // Port
 const PORT = process.env.Port || 3001;
 
-// Parse incoming requests data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+let authPort = '';
 
-// Routes
-app.use(gatewayRoutes);
-
-app.listen(PORT, () => {
-    // eslint-disable-next-line no-console
-    console.log(`Server is running on Port ${PORT}`);
+docker.listContainers((err, containers) => {
+  containers.forEach((containerInfo) => {
+    if (containerInfo.Names[0] === '/auth-service' || containerInfo.Names[0] === '/meal-service') {
+      console.log(containerInfo.Ports[0].PublicPort);
+      const metaContainer = {
+        route: containerInfo.Labels.apiRoute,
+        port: containerInfo.Ports[0].PublicPort,
+      };
+      if (containerInfo.Labels.apiRoute.includes('users')) {
+        authPort = containerInfo.Ports[0].PublicPort;
+      }
+      containerMetaData.push(metaContainer);
+    }
+  });
+  mediator.emit('onComplete');
 });
 
-export default app
+mediator.on('onComplete', () => {
+  const authMiddleware = middlewareObj(authPort);
+  containerMetaData.forEach((value) => {
+    const { route, port } = value;
+    const target = `http://0.0.0.0:${port}`;
+    app.use(
+      route,
+      authMiddleware.authenticationMiddleWare,
+      proxy({ target, changeOrigin: true }),
+    );
+  });
+  app.listen(PORT, () => {
+    console.log(`listening at port ${PORT}`);
+  });
+});
+
+export default app;
