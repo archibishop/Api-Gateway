b4797d8a1a3a0952a68d4f7b89be7c97e340d393
Wagubi Brian <wagubib@gmail.com>
2019-11-05 18:29:52 +0300

    refactor api-gateway.

--- api/middleware/isAuthroized.js
+++ api/middleware/isAuthroized.js
@@ -1,52 +1,61 @@
 import axios from 'axios';
 
-const isAuthroized = async (req, res, next) => {
-    const token = req.headers['x-access-token'];
-    const refreshToken = req.headers['refresh-token'];
-    const headers = {
-        'x-access-token': token
-    }
-    try {
-        if (token && refreshToken) {
-            const response = await axios.get('http://0.0.0.0:3070/api/v1/users/secure', { headers });
-            next()
-        } else {
-            res.send({
-                success: false,
-                message: "Please provide token and refresh-token for authentication.",
-            });
-        }
-    } catch(err){
-        const { data, status }  = err.response
-        if (data.message === 'jwt expired'){
-            console.log("token has expired");
-            try {
-                const response = await axios.post('http://0.0.0.0:3070/api/v1/users/refresh-token', { refreshToken });
-                // TODO ::
-                // Use web sockets to send a push to the front-end.
-                console.log(response.data);
-                // Proceed with the request since a new token has been generated and sent to the front-end.
-                next();
-            } catch (err) {
-                res.send({
-                    success: false,
-                    message: "Autehntication failed.",
-                    status,
-                    data
-                });
-            }
-            res.send({
-                message: "Testing refresh tokens."
-            });
-        } else  {
-            res.send({
-                success: false,
-                message: "Autehntication failed.",
-                status,
-                data
-            });
-        }
-    }
-}
+const middleWareObj = (authPort) => {
+  const unAuthenticatedRoutes = ['/api/v1/users/login', '/api/v1/users', '/api/v1/users/secure'];
 
-export default isAuthroized;
+  const authenticationMiddleWare = async (req, res, next) => {
+    if (unAuthenticatedRoutes.includes(req.originalUrl)) {
+      next();
+    } else {
+      const token = req.headers['x-access-token'];
+      const refreshToken = req.headers['refresh-token'];
+      const headers = {
+        'x-access-token': token,
+      };
+      try {
+        if (token && refreshToken) {
+          await axios.get(`http://0.0.0.0:${authPort}/api/v1/users/secure`, { headers });
+          next();
+        } else {
+          res.send({
+            success: false,
+            message: 'Please provide token and refresh-token for authentication.',
+          });
+        }
+      } catch (err) {
+        const { data, status } = err.response;
+        if (data.message === 'jwt expired') {
+          try {
+            await axios.post(`http://0.0.0.0:${authPort}/api/v1/users/refresh-token`, { refreshToken });
+            // TODO ::
+            // Use web sockets to send a push to the front-end.
+            // Proceed with the request since a new token has been generated and sent to front-end.
+            next();
+          } catch (error) {
+            res.send({
+              success: false,
+              message: 'Autehntication failed.',
+              status,
+              data,
+            });
+          }
+          res.send({
+            message: 'Testing refresh tokens.',
+          });
+        } else {
+          res.send({
+            success: false,
+            message: 'Autehntication failed.',
+            status,
+            data,
+          });
+        }
+      }
+    }
+  };
+  return Object.create({
+    authenticationMiddleWare,
+  });
+};
+
+export default middleWareObj;
